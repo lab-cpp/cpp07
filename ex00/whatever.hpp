c4b753cbe3b9f681c89d81dfef13235b0d8b8c2e
alcacere <alcacere@localhost.com>
2026-09-02 03:15:29 +0200

    formating code

--- ex00/whatever.hpp
+++ ex00/whatever.hpp
@@ -10,12 +10,16 @@ void swap(T& param1, T& param2)
     param2 = tmp;
 }
 
-template <typename T>const T& min(const T& param1, const T& param2) {
-	return param1 < param2 ? param1 : param2;
+template <typename T>
+const T& min(const T& param1, const T& param2)
+{
+    return param1 < param2 ? param1 : param2;
 }
 
-template <typename T>const T& max(const T& param1, const T& param2) {
-	return param1 > param2 ? param1 : param2;
+template <typename T>
+const T& max(const T& param1, const T& param2)
+{
+    return param1 > param2 ? param1 : param2;
 }
 
 #endif
